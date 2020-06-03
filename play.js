37b41681f28e775f95ddcc4e85f59d05e31a5e22
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-03 17:55:52 +0000

    Refactor, and moved code into input.js

--- play.js
+++ play.js
@@ -1,21 +1,8 @@
 const connect = require('./client');
-
+const setupInput = require('./input')
 console.log('Connecting ...');
 
 connect();
-const handleUserInput = (data) => {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-//Setup user interface to handle input
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();Is
-  return stdin;
-};
 
-const input = setupInput()
-input.on("data", handleUserInput)
+
+setupInput();
