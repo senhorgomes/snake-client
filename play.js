fc93aac2f7ecc96c7d266e8a1c3ff7af41492ea2
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-03 16:46:24 +0000

    Added connection to host, and server output.

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+//Establishes connection with the game server
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "135.23.222.148",
+    port: 50541
+  });
+  //Interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  return conn;
+};
+
+console.log('Connecting...');
+connect();
