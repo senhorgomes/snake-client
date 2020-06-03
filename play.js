6c77d987c415c8241b9560454e7cd779ac436000
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-03 17:44:04 +0000

    Added proper Ctrl+C function

--- play.js
+++ play.js
@@ -3,17 +3,19 @@ const connect = require('./client');
 console.log('Connecting ...');
 
 connect();
-
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
 //Setup user interface to handle input
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = () => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
+  stdin.resume();Is
   return stdin;
 };
+
+const input = setupInput()
+input.on("data", handleUserInput)
